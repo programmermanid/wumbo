396a2abf67536d800bdbbb6e8cb66a41e9f635ba
Fajar Windhu Zulfikar <fajarwindhuzulfikar@gmail.com>
2022-02-28 12:29:45 +0700

    code: full

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: '#193766',
+        'badge-primary': '#1C3E74',
+        'badge-primary-light': '#EBF3FE',
+        'border': '#24416E',
+        'copyright': '#7587A3',
+        'dark': '#193766',
         'dark-light': '#2B4772',
-        muted: '#697B98',
-        primary: '#3B82F6',
+        'muted': '#697B98',
+        'muted-light': '#FAFAFA',
+        'primary': '#3B82F6',
         'primary-light': '#C6DBFF',
-        secondary: '#E4EFFF',
+        'secondary': '#E4EFFF',
+        'tertiary': '#1DA1F2',
       },
       width: {
         '600px': '600px',
